ad195161aabb31f1afa753fc41f9c86c70509777
Linda Mansour <124932177+zawmbi@users.noreply.github.com>
2026-04-25 23:51:18 -0500

    Revert Cloudflare adapter, return to static build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import cloudflare from "@astrojs/cloudflare";
+
 export default defineConfig({
   site: 'https://zawmbi.github.io',
   trailingSlash: 'ignore',
+
   build: {
     format: 'directory',
   },
+
+  adapter: cloudflare()
 });
